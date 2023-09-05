62e925e756f4e429d279f8ed547b275338fb4204
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-09-05 14:04:17 +0200

    css

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,13 @@
+import {Paragraph} from './Statistics.styled'
 export const Statistics = ({ good, neutral, bad, total, percentage }) => {
   return (
     <>
       <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Positive feedback: {percentage} %</p>
+      <Paragraph>Good: {good}</Paragraph>
+      <Paragraph>Neutral: {neutral}</Paragraph>
+      <Paragraph>Bad: {bad}</Paragraph>
+      <Paragraph>Total: {total}</Paragraph>
+      <Paragraph>Positive feedback: {percentage} %</Paragraph>
     </>
   );
 };
